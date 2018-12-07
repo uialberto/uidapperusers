a95b19843ca9e33eac01cf27980372e0a93645e2
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2018-12-06 22:11:50 -0400

    implementacion unitofwork repository with dapper

--- SolutionUibasoft/App.Console/Program.cs
+++ SolutionUibasoft/App.Console/Program.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Community.AppServices.Modulos.Seguridad;
 using Community.AppServices.Modulos.Seguridad.Impl;
+using Community.DataAccess.Infrastructure;
+using Community.DataAccess.Repositorios.Seguridad;
+using Community.DataAccess.Repositorios.Seguridad.Impl;
+using Community.DataAccess.UnitOfWork;
 using Community.DataTransfer.DataTransferObjects.Modulos.Seguridad;
+using Unity;
 
 namespace App.Console
 {
@@ -12,8 +18,30 @@ namespace App.Console
     {
         static void Main(string[] args)
         {
+            var container = new UnityContainer();
+            container.RegisterType<IRepoUsuarios, RepoUsuariosGeneric>();
+            container.RegisterType<IConnectionFactory, ConnectionFactory>();
+            container.RegisterType<IUnitOfWork, UnitOfWorkDapper>();
+            container.RegisterType<IServiceUsuarios, ServiceUsuariosGeneric>();
+
+            var appUsers = container.Resolve<IServiceUsuarios>();
+
+            var task = Task.Run(() => appUsers.GetAllPaginate(1,1));
+            task.Wait();          
+
+            var usuarios = task.Result;
+
+            foreach (var user in usuarios)
+            {
+                System.Console.WriteLine($"Id: {user.Id}, Nombres :{user.Nombres}, Apellidos: {user.Apellidos}");
+            }
+            System.Console.ReadLine();
+
+        }
+        private void RunDapper()
+        {
             GetAll();
-            var newUsuario = new UsuarioDto(){ Nombres = "Alberto 2", Apellidos = "Baigorria 2"};        
+            var newUsuario = new UsuarioDto() { Nombres = "Alberto 2", Apellidos = "Baigorria 2" };
             var row = new ServiceUsuarios().Insert(newUsuario);
             System.Console.WriteLine($"Filas Insertadas: {row}");
             GetAll();
--- SolutionUibasoft/Community.DataAccess/UnitOfWork/IUnitOfWork.cs
+++ SolutionUibasoft/Community.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace Community.DataAccess.UnitOfWork
 {
     public interface IUnitOfWork : IDisposable
     {
+        https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
         IRepoUsuarios UsuariosRepo { get; }
         void Complete();
     }
